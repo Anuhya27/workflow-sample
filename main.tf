8f76a417f36a3f37ce9184d995873641659d4767
Anuhya27 <anuhya1727@gmail.comm>
2024-01-03 16:51:01 +0530

    Updated tf

--- main.tf
+++ main.tf
@@ -1,66 +1,88 @@
-variable "region" {}
-variable "vpc_name" {}
-
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>3.10"
+      Version = "~>3.27"
     }
   }
+
   required_version = ">=0.14.9"
 
 }
 
 provider "aws" {
-    region = "us-east-1"
+  version = "~>3.0"
+  region  = "east-us-1"
 }
 
 
-resource "aws_instance" "my-first-server" {
-    ami = "ami-079db87dc4c10ac91"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "Second Instance"
-    }
-}
 
-# data "aws_ami" "latest_amazon_linux" {
-#     most_recent = true
-#     owners = ["amazon"]
-#     filter {
-#         name = "name"
-#         values= ["amzn2-ami-hvm-*"]
+
+
+##############################################
+# variable "region" {}
+# variable "vpc_name" {}
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "~>3.10"
 #     }
-#     filter {
-#         name = "architecture"
-#         values = ["x86_64"]
-#     }
-#     filter {
-#         name = "root-device-type"
-#         values = ["ebs"]
-#     }
+#   }
+#   required_version = ">=0.14.9"
+
 # }
 
-# data "aws_vpc" "selected_vpc" {
-# 	filter {
-# 	   name = "tag:Name"
-# 	   values = [var.vpc_name]
-# 	}
+# provider "aws" {
+#     region = "us-east-1"
 # }
 
-# data "aws_subnets" "private" {
-# 	filter {
-# 	   name = "vpc-id"
-# 	   values = [data.aws_vpc.selected_vpc.id]
-# 	}
-# 	tags = {
-# 		Type = "private"
-# 	}
-# }
 
-# resource "aws_instance" "ec2_instance" {
-# 	ami = data.aws_ami.latest_amazon_linux.id
+# resource "aws_instance" "my-first-server" {
+#     ami = "ami-079db87dc4c10ac91"
 #     instance_type = "t2.micro"
-# 	subnet_id = data.aws_subnets.private.ids[0]
+#     tags = {
+#         Name = "Second Instance"
+#     }
 # }
+
+# # data "aws_ami" "latest_amazon_linux" {
+# #     most_recent = true
+# #     owners = ["amazon"]
+# #     filter {
+# #         name = "name"
+# #         values= ["amzn2-ami-hvm-*"]
+# #     }
+# #     filter {
+# #         name = "architecture"
+# #         values = ["x86_64"]
+# #     }
+# #     filter {
+# #         name = "root-device-type"
+# #         values = ["ebs"]
+# #     }
+# # }
+
+# # data "aws_vpc" "selected_vpc" {
+# # 	filter {
+# # 	   name = "tag:Name"
+# # 	   values = [var.vpc_name]
+# # 	}
+# # }
+
+# # data "aws_subnets" "private" {
+# # 	filter {
+# # 	   name = "vpc-id"
+# # 	   values = [data.aws_vpc.selected_vpc.id]
+# # 	}
+# # 	tags = {
+# # 		Type = "private"
+# # 	}
+# # }
+
+# # resource "aws_instance" "ec2_instance" {
+# # 	ami = data.aws_ami.latest_amazon_linux.id
+# #     instance_type = "t2.micro"
+# # 	subnet_id = data.aws_subnets.private.ids[0]
+# # }
