eca8c28b22b1af2718ede32ef333239155e02881
Anuhya27 <anuhya1727@gmail.comm>
2024-01-03 16:17:59 +0530

    version 6

--- main.tf
+++ main.tf
@@ -5,16 +5,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
     }
   }
-
   required_version = ">=0.14.9"
 
 }
 
 provider "aws" {
-    version = "~>3.0"
+    version = "~>1.0"
     region = "us-east-1"
 }
 
