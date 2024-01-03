fe73213f16c0d825185a83d7c1ed77a6171d90fc
Anuhya27 <anuhya1727@gmail.comm>
2024-01-03 16:12:18 +0530

    version 6

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
 variable "region" {}
 variable "vpc_name" {}
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
 provider "aws" {
+    version = "~>3.0"
     region = "us-east-1"
 }
 
