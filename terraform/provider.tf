06c14a1d9ab36c0ab041ea248c50f17a523c983f
Anuhya27 <anuhya1727@gmail.comm>
2024-01-24 14:07:03 +0530

    Getting aws region through secrets

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -15,12 +15,17 @@ terraform {
   required_version = "~> 1.0"
 }
 
+variable "AWS_REGION" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"  # Set a default region or leave it empty
+}
+
 provider "aws" {
     var.AWS_REGION
   # Other provider configuration options, if needed
 }
 
-variable "AWS_REGION" {
-  description = "AWS region to deploy resources"
-  default     = "us-east-1"  # Set a default region or leave it empty
+
+output "AWS_REGION" {
+    value = var.AWS_REGION
 }
