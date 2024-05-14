c7e20b7e9479343d8bf915259e5202af2935200a
Anuhya27 <anuhya1727@gmail.com>
2024-05-14 13:48:25 +0530

    Get env variable

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,6 @@
 ########### PROGRAM ASSESSMENT ECR REPOSITORY ##############
 resource "aws_ecr_repository" "program_assessment_repo" {
-  name                 = var.ecr_image_name
+  name                 = var.JOB_DEFINITION
   image_tag_mutability = "IMMUTABLE"
 }
 
