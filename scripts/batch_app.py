4159fbf70b706f5801316a879d0f496cff702112
Anuhya Sarabu <anuhya_test@gmail.com>
2024-07-30 11:02:43 +0530

    ecr synk

--- scripts/batch_app.py
+++ scripts/batch_app.py
@@ -1,11 +1,14 @@
-import argparse
+# import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--user_input")
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--user_input")
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-if args.user_input is None:
-    print('The user input is none.')
-else:
-    print(f'The user input is {args.user_input}')
+# if args.user_input is None:
+#     print('The user input is none.')
+# else:
+#     print(f'The user input is {args.user_input}')
+
+def test():
+    print("Hello world!!")
