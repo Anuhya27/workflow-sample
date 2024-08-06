d743f4d9ad1c4b3481104431bfd9c9f5a59f2701
Anuhya Sarabu <anuhya_test@gmail.com>
2024-08-06 13:31:47 +0530

    lambda_docker

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,26 +1,26 @@
-# ########### PROGRAM ASSESSMENT ECR REPOSITORY ##############
-# resource "aws_ecr_repository" "program_assessment_repo" {
-#   name                 = var.RESOURCE_NAME
-#   image_tag_mutability = "IMMUTABLE"
-#   encryption_configuration {
-#     encryption_type = "AES256"  # Default encryption type
-#   }
-#   tags = {
-#     "Name"               = var.Name
-#     "Cost_Center_Name"   = var.Cost_Center_Name
-#     "Department"         = var.Department
-#     "epi:team"           = var.Team
-#     "epi:supported-by"   = var.SupportedBy
-#     "epi:owner"          = var.Owner
-#     "epi:environment"    = var.Environment
-#     "epi:product-stream" = var.ProductStream
-#   }
-# }
+########### PROGRAM ASSESSMENT ECR REPOSITORY ##############
+resource "aws_ecr_repository" "program_assessment_repo" {
+  name                 = var.RESOURCE_NAME
+  image_tag_mutability = "IMMUTABLE"
+  encryption_configuration {
+    encryption_type = "AES256"  # Default encryption type
+  }
+  tags = {
+    "Name"               = var.Name
+    "Cost_Center_Name"   = var.Cost_Center_Name
+    "Department"         = var.Department
+    "epi:team"           = var.Team
+    "epi:supported-by"   = var.SupportedBy
+    "epi:owner"          = var.Owner
+    "epi:environment"    = var.Environment
+    "epi:product-stream" = var.ProductStream
+  }
+}
 
-# output "repository_url" {
-#   value = aws_ecr_repository.program_assessment_repo.repository_url
-# }
+output "repository_url" {
+  value = aws_ecr_repository.program_assessment_repo.repository_url
+}
 
-# output "ECR_REPO_NAME" {
-#   value = aws_ecr_repository.program_assessment_repo.name
-# }
+output "ECR_REPO_NAME" {
+  value = aws_ecr_repository.program_assessment_repo.name
+}
