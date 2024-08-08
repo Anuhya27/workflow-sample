2dda7c23725b9372d8b5ac00c2c183925e049409
Anuhya Sarabu <anuhya_test@gmail.com>
2024-08-08 13:49:37 +0530

    finla test

--- lambda_handler.py
+++ lambda_handler.py
@@ -76,7 +76,7 @@ def lambda_handler(event, context):
     
     source_region = 'us-west-2'
     dest_region = 'us-east-1'
-    slack_webhook_url = "https://hooks.slack.com/services/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"  # Replace with your Slack Webhook URL
+    slack_webhook_url = "https://hooks.slack.com/services/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"  # Replace with your Slack Webhook URL
     source_ecr = boto3.client('ecr', region_name=source_region)
     dest_ecr = boto3.client('ecr', region_name=dest_region)
 
