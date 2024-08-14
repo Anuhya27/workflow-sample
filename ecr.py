f1a03a5ca1f9317c19088b398413c4af73a6700b
Anuhya Sarabu <anuhya_test@gmail.com>
2024-08-14 19:18:38 +0530

    ecr check

--- ecr.py
+++ ecr.py
@@ -5,8 +5,10 @@ def main(name):
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Prints a personalized greeting.")
-    parser.add_argument("--name", required=True, help="Name to greet")
+    # parser.add_argument("--name", required=True, help="Name to greet")
+    parser.add_argument(
+        "--coding_type", nargs="+", required=True, help="List of coding types"
+    )
     args = parser.parse_args()
-
-    main(args.name)
-    
+    print(str(args.coding_type))
+    # main(args.name)
