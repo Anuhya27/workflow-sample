d2b31510e6136e40a547e844647901b395ce9a4e
AnuhyaSarabu <anuhya.sarabu@episourceindia.com>
2024-09-12 16:33:45 +0530

    Add scheduler

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -7,7 +7,7 @@ resource "aws_scheduler_schedule" "cron" {
     mode = "OFF"
   }
 
-  schedule_expression          = "cron(15 16 ? * 5 *)"
+  schedule_expression          = "cron(35 16 ? * 5 *)"
   schedule_expression_timezone = "Asia/Calcutta" # Default is UTC
   description                  = "submitJob Batch event"
 
