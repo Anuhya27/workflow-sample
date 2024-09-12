10cd5c8ca43645324783231b4e6f16acfdaf53f7
AnuhyaSarabu <anuhya.sarabu@episourceindia.com>
2024-09-12 12:08:54 +0530

    Add scheduler

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -7,13 +7,13 @@ resource "aws_scheduler_schedule" "cron" {
     mode = "OFF"
   }
 
-  schedule_expression          = "cron(06 12 ? * 5 *)"
+  schedule_expression          = "cron(12 12 ? * 5 *)"
   schedule_expression_timezone = "Asia/Calcutta" # Default is UTC
   description                  = "submitJob Batch event"
 
   target {
     arn      = "arn:aws:scheduler:::aws-sdk:batch:submitJob"
-    role_arn = var.iam_role_arn
+    role_arn = "arn:aws:iam::050223225208:role/scheduler_role"
 
     input = jsonencode({
       "JobName" : "${aws_batch_job_definition.program_assessment.name}",
